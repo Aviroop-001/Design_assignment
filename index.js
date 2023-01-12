cce2ee09d030a3c2aa36c21604d50d1c0a44e518
Aviroop-001 <banerjeeaviroop01@gmail.com>
2023-01-12 20:48:13 +0530

    Inital commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function started(){
+    alert("Staring Docs Downloaded")
+}
+
+function login() {
+  alert("You will be redirected to Login Page");
+}
+
+function signup() {
+  alert("User signed up successfully");
+}
+
+function feedback() {
+  alert("Thanks for the feedback ;)");
+}
